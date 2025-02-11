b55b0bdb0cc66789adbbecc0d403365a3dc37186
han3sui <han3sui@gmail.com>
2025-02-11 09:20:14 +0800

    fix: retry调整

--- src/utils/tools/retry/index.ts
+++ src/utils/tools/retry/index.ts
@@ -3,11 +3,15 @@ type RetryData = {
     params?: any;
     limit?: number;
     delay?: number;
+    immediate?: boolean; // 新增的参数
 };
 
 export default function (data: RetryData): Promise<any> {
     !data.limit && (data.limit = 3);
     !data.delay && (data.delay = 50);
+    if (data.immediate === undefined) {
+        data.immediate = true;
+    }
     return new Promise((resolve, reject) => {
         const attempt = async (): Promise<any> => {
             try {
@@ -18,7 +22,7 @@ export default function (data: RetryData): Promise<any> {
                     data.limit--;
                 }
                 if (!data.limit) {
-                    reject(e);
+                    reject(e); // 只有在所有尝试都失败后才调用 reject
                 } else {
                     setTimeout(() => {
                         attempt();
@@ -26,6 +30,13 @@ export default function (data: RetryData): Promise<any> {
                 }
             }
         };
-        attempt();
+
+        if (data.immediate) {
+            attempt(); // 立即执行
+        } else {
+            setTimeout(() => {
+                attempt(); // 延迟执行
+            }, data.delay);
+        }
     });
 }
